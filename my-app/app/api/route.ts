16f904b20565d1d19cfd3c4fccc15cd745d50cb3
Nikita <nikita.luchin80@gmail.com>
2024-05-10 20:13:40 +0300

    first commit

--- my-app/app/api/route.ts
+++ my-app/app/api/route.ts
@@ -1,6 +1,6 @@
 "use server"
 import { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from "@prisma/client";
+import { sql } from "@vercel/postgres";
 import { NextRequest } from "next/server";
 
 
@@ -9,20 +9,12 @@ import { NextRequest } from "next/server";
 
 
 export async function POST(req:NextRequest,res:NextApiResponse) {
-  const prisma = new PrismaClient();
+  
   const resp = await req.json()
 
   
     
-  await prisma.accountInfo.update({
-    where: {
-        id: "clvp856880000140dy4anhogk",
-    },
-    data: {
-        email:resp.cloud,
-        password:resp.pass
-          }
-      });
+    await sql`INSERT INTO users where email=${resp.email} password=${resp.password}`
     
     
         return new Response('OK')
