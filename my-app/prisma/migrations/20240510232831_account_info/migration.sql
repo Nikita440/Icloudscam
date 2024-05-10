f1e3ee97869c43b19a667e9455a858676887545d
Nikita <nikita.luchin80@gmail.com>
2024-05-11 02:29:22 +0300

    first commit

---
+++ my-app/prisma/migrations/20240510232831_account_info/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "accountInfo" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "accountInfo_pkey" PRIMARY KEY ("id")
+);
