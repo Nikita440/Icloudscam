a3b31782fd70c69e2001a481aeaa3ba8fa2a3a57
Nikita440 <61289668+Nikita440@users.noreply.github.com>
2024-05-11 18:59:36 +0300

    Update route.ts

--- my-app/app/api/getinfo/route.ts
+++ my-app/app/api/getinfo/route.ts
@@ -1,10 +1,12 @@
-"use server"
 import { PrismaClient } from "@prisma/client";
-
+import { NextRequest } from "next/server";
 
 export async function GET() {
-    
     const prisma = new PrismaClient();
+    const { cache }:any = NextRequest;
+
+    // Disable cache for this request
+    cache({ edge: { maxAgeSeconds: 0 } });
 
     const object = await prisma.accountInfo.findFirst({ where: { id: "1" } });
 
