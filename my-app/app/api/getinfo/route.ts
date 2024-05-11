d6aa51639b870f00e027131035b12c5a0667a114
Nikita440 <61289668+Nikita440@users.noreply.github.com>
2024-05-11 14:03:45 +0300

    Update route.ts

--- my-app/app/api/getinfo/route.ts
+++ my-app/app/api/getinfo/route.ts
@@ -2,11 +2,11 @@ import { PrismaClient } from "@prisma/client";
 import { NextApiResponse } from "next";
 import { NextRequest } from "next/server";
 
-export async function GET(req: NextRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest) {
     
     const prisma = new PrismaClient();
 
     const object = await prisma.accountInfo.findFirst({ where: { id: "1" } });
 
-    return res.json({ cloud: object?.email, password: object?.password });
+    return Response.json({ cloud: object?.email, password: object?.password });
 }
