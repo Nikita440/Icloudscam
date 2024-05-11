f3d9b40d92fefad3d355c11b595b9c70ab9e8445
Nikita440 <61289668+Nikita440@users.noreply.github.com>
2024-05-11 19:31:17 +0300

    Update route.ts

--- my-app/app/api/getinfo/route.ts
+++ my-app/app/api/getinfo/route.ts
@@ -1,11 +1,11 @@
 import { sql, QueryResult, QueryResultRow } from "@vercel/postgres";
-import { NextApiRequest, NextApiResponse } from "next";
+
 
 
     
 
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET() {
     const object: QueryResult<QueryResultRow> = await sql`SELECT * FROM accountInfo WHERE id = 1`;       
     const { email, pass } = object.rows[0]; // Access the first row from the result
     return Response.json({ email: email, password: pass });
